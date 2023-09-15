8585ebc58509aebfe1b4ab4b92fad7eea99313a8
Calvin Zhu <calvinzhu@outlook.com>
2023-09-15 14:01:50 -0600

    Implemented the ai preformance monitoring

--- calvin-demo/apps/frontend-ui/README.md
+++ calvin-demo/apps/frontend-ui/README.md
@@ -13,9 +13,16 @@ npm i @angular/material @angular/cdk --save
 ### Step 3: install dependiency
 ```
 npm install @microsoft/applicationinsights-web@latest
+npm i @microsoft/applicationinsights-perfmarkmeasure-js --save
 ```
 
 ## Reference
 [Monitoring Live Angular Apps with Application Insights](https://onthecode.co.uk/blog/monitoring-live-angular-apps-with-azure-application-insights)
 
 [Angular How-to: Add Application Insights to an Angular SPA](https://devblogs.microsoft.com/premier-developer/angular-how-to-add-application-insights-to-an-angular-spa/)
+
+[Microsoft Application Insights JavaScript SDK - Performance Mark and Measure Plugin](https://github.com/microsoft/ApplicationInsights-JS/blob/main/extensions/applicationinsights-perfmarkmeasure-js/README.md)
+
+[Monitoring preformance](https://github.com/microsoft/ApplicationInsights-JS/blob/master/docs/PerformanceMonitoring.md)
+
+[Debug plugin extension (Beta)](https://github.com/microsoft/ApplicationInsights-JS/blob/master/extensions/applicationinsights-debugplugin-js/README.md)
--- calvin-demo/apps/frontend-ui/src/app/services/monitoring.service.ts
+++ calvin-demo/apps/frontend-ui/src/app/services/monitoring.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from "@angular/core";
 import { ApplicationInsights, SeverityLevel } from "@microsoft/applicationinsights-web";
+import { PerfMarkMeasureManager } from '@microsoft/applicationinsights-perfmarkmeasure-js';
 //import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: "root" })
 export class MonitoringService {
+  
   constructor(private readonly appInsights: ApplicationInsights) {
     this.appInsights.loadAppInsights();
+
+    // window.performance will now contain 
+    // - a mark called 'ai.prfmrk.code'
+    // - a measure called 'ai.prf.msr.code'
+    // monitoring preformance in debug plugin extension (Beta)
+    // https://github.com/microsoft/ApplicationInsights-JS/blob/master/extensions/applicationinsights-debugplugin-js/README.md
+    // More Details:
+    // https://github.com/microsoft/ApplicationInsights-JS/blob/master/docs/PerformanceMonitoring.md
+    this.appInsights.core.setPerfMgr(new PerfMarkMeasureManager());
 
     /*
     this.appInsights.setAuthenticatedUserContext("USER_EMAIL");
